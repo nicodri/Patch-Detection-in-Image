1e4680257d93d414a7ba796e1e42f55dcef3f81d
nicodri <nicodrizard@hotmail.com>
2015-01-30 17:43:53 +0100

    patch matching

--- patch_matching.py
+++ patch_matching.py
@@ -4,4 +4,31 @@ Created on Fri Jan 30 14:13:01 2015
 
 @author: drizardnicolas
 """
+from scipy import misc
+import numpy as np
+
+#load images in fray-scale layer
+img = misc.imread('test.png',1)
+eye=misc.imread('eye.png',1)
+
+#-------------------Estimate the patch
+#set the list of the data input
+input=np.zeros((3,eye.shape[0]*eye.shape[1]))
+c=0
+for i in range(eye.shape[0]):
+    for j in range(eye.shape[1]):
+        input[0,c]=i
+        input[1,c]=j
+        input[2,c]=eye[i,j]
+        c+=1
+        
+#mean
+mu_eye=[]
+mu_eye.append(eye.shape[0]/2)
+mu_eye.append(eye.shape[1]/2)
+mu_eye.append(np.mean(eye))
+
+#covariance
+cov_eye=np.cov(input)
+
 
