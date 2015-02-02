ea940cb17b290db767249f87960a70709ec02b5b
nicodri <nicodrizard@hotmail.com>
2015-02-02 19:20:32 +0100

    changed

--- patch_matching.py
+++ patch_matching.py
@@ -34,9 +34,16 @@ cov_eye=np.cov(input)
 
 #--------set the summed area table
 #sum to compute mu
-I_sum=np.zeros((img.shape[0],img.shape[1],3))
-I_sum[0,0,:]=[0,0,img[0,0]]
+I_sum=np.zeros((img.shape[0],img.shape[1]))
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
-        I_sum[]
+        I_sum[i,j]=img[i,j]+(I_sum[i-1,j] if i>0 else 0)+(I_sum[i,j-1] if j>0 else 0)-(I_sum[i-1,j-1] if i>0 and j>0 else 0)
 
+#sum to compute cov
+I_cov=np.zeros((img.shape[0],img.shape[1],3,3))
+temp=np.zeros((3,3))
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        X=[[i,j,img[i,j]]]
+        temp=np.transpose(X)*X
+        I_cov[i,j,:,:]=temp+(I_cov[i-1,j,:,:] if i>0 else 0)+(I_cov[i,j-1,:,:] if j>0 else 0)-(I_cov[i-1,j-1,:,:] if i>0 and j>0 else 0)
